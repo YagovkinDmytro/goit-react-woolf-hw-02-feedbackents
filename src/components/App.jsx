5709b2a9abaa07195208c1f78900a0dc44326d99
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-04-05 19:27:10 +0200

    step-1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './Statistics/FeedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positiveFeedback: 0,
+  };
+
+  hendlerclickStatistic = () => {
+    this.setState(prevState => {
+      // return {
+      //   [option]: prevState[option] + 1,
+      // };
+    });
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+
+  countTotalFeedback = () => {
+    this.setState(prevState => ({
+      total: prevState.countGood + prevState.countNeutral + prevState.countBad,
+    }));
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    this.setState(prevState => ({
+      positiveFeedback: Math.floor(
+        (prevState.countGood * 100) / prevState.total
+      ),
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.hendlerclickStatistic}
+        />
+        <Statistics
+          good={this.state.countGood}
+          neutral={this.state.countNeutral}
+          bad={this.state.countBad}
+          total={this.state.total}
+          positivePercentage={this.state.positiveFeedback}
+        />
+      </div>
+    );
+  }
+}
+
+//**Step 1, 2 before refactoring**//
+// import Statistics from './Statistics/Statistics';
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Statistics></Statistics>
+//     </div>
+//   );
+// };
---
+++ src/components/Statistics/FeedbackOptions.jsx
@@ -0,0 +1,13 @@
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  console.log(options);
+  return (
+    <div>
+      <h2>Please leave feedback</h2>
+      {/* <button onClick={}>Good</button>
+      <button onClick={}>Neutral</button>
+      <button onClick={}>Bad</button> */}
+    </div>
+  );
+};
+
+export default FeedbackOptions;
